c0d2d9605debc6ad01a35da42fedb3a688c92602
yopen24 <61549437+yopen24@users.noreply.github.com>
2020-11-17 20:04:56 -0300

    connecting to prod DB

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "diego",
-    password: "prueba",
-    database: "eyesUpHere",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
